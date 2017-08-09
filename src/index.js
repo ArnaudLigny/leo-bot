1cc1bbc092494e668cddc250b5f46951cd74dc7e
Arnaud Ligny <arnaud@ligny.org>
2017-08-09 15:27:13 +0200

    Update messages.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@
 const config = require('../config.json')[process.env.NODE_ENV || 'development']
 const schedule = require('node-schedule')
 const token = process.env.SLACK_BOT_TOKEN
-const text = 'Attention <!channel>, il est temps de s\'organiser pour récupérer Léo !'
+const message_reminder = 'Attention <!channel>, il est temps de s\'organiser pour récupérer Léo !'
+const message_help = 'Hello, je ne sers que de rappel journalier ! :smile:'
 
 if (!token) {
   console.log('Error: Specify token in environment')
@@ -36,7 +37,7 @@ schedule.scheduleJob(config['SCHEDULE'], function () {
   bot.api.chat.postMessage({
     'token': token,
     'channel': config['CHANNEL'],
-    'text': text,
+    'text': message_reminder,
     'as_user': true
   })
 })
@@ -45,7 +46,5 @@ schedule.scheduleJob(config['SCHEDULE'], function () {
  * Explain the user how to use the bot
  */
 controller.hears(['aide', 'help'], 'direct_message,direct_mention,mention', function (bot, message) {
-  bot.reply(message,
-    'Hello, je ne sers que de rappel journalier ! :smile:\n'
-  )
+  bot.reply(message, message_help)
 })
